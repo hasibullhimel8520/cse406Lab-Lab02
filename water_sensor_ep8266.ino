4c4228c33e1bd316e486bb1aa2f9070e6e1e113d
hasibullhimel8520 <hasibullhimel8520@gmail.com>
2025-10-13 12:18:47 +0600

    Add files via upload

---
+++ water_sensor_ep8266.ino
@@ -0,0 +1,31 @@
+/*
+ * This ESP8266 NodeMCU code was developed by newbiely.com
+ *
+ * This ESP8266 NodeMCU code is made available for public use without any restriction
+ *
+ * For comprehensive instructions and wiring diagrams, please visit:
+ * https://newbiely.com/tutorials/esp8266/esp8266-water-sensor
+ */
+
+#define POWER_PIN  D7
+#define SIGNAL_PIN A0
+
+int value = 0; // variable to store the sensor value
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(POWER_PIN, OUTPUT);   // Configure D7 pin as an OUTPUT
+  digitalWrite(POWER_PIN, LOW); // turn the sensor OFF
+}
+
+void loop() {
+  digitalWrite(POWER_PIN, HIGH);  // turn the sensor ON
+  delay(10);                      // wait 10 milliseconds
+  value = analogRead(SIGNAL_PIN); // read the analog value from sensor
+  digitalWrite(POWER_PIN, LOW);   // turn the sensor OFF
+
+  Serial.print("Sensor value: ");
+  Serial.println(value);
+
+  delay(1000);
+}
